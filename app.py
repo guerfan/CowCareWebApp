22ac0fcb3a15a0e2f5dda412b7272ed20ca3e54f
AllenYu <Xyuskie@gmail.com>
2016-03-23 19:15:07 -0400

    Current-ish

--- app.py
+++ app.py
@@ -14,16 +14,16 @@ base_url = 'http://localhost:10200/v1'
 #base_url = 'https://katys-care-api.herokuapp.com/v1'
 
 
-# login required decorator
-def login_required(f):
-    @wraps(f)
-    def wrap(*args, **kwargs):
-        if 'logged_in' in session:
-            return f(*args, **kwargs)
-        else:
+# # login required decorator
+# def login_required(f):
+#     @wraps(f)
+#     def wrap(*args, **kwargs):
+#         if 'logged_in' in session:
+#             return f(*args, **kwargs)
+#         else:
             
-            return redirect(url_for('login'))
-    return wrap
+#             return redirect(url_for('login'))
+#     return wrap
 
 
 # use decorators to link the function to a url
@@ -46,6 +46,7 @@ def login():
             if login_status.status_code == 200:
                 token = json.loads(login_status.text)['data']['id']
                 session['token'] = token
+                session['id'] = data['email']
                 return redirect(url_for('home'))
             else:
                 error = 'Invalid Credentials. Please try again.'
@@ -79,7 +80,7 @@ def register():
 # @login_required
 def logout():
     session.pop('token', None)
-    return redirect(url_for('home'))
+    return redirect(url_for('login'))
 
 # route for handling displaying treatment plans list
 @app.route('/treatment_plans')
@@ -91,20 +92,37 @@ def treatmentplan():
     return render_template('treatmentplan.html',data=raw)
 
 # route for handling displaying list of cows
-@app.route('/cows')
+@app.route('/calves')
 def cowList():
     auth = {}
-    auth['Authorization'] = session['token']
-    cow_list_status = requests.get("{url}/farms".format(url=base_url),headers = auth)
-    return cow_list_status.text
+    if session.has_key('token') == True:
+        auth['Authorization'] = session['token']
+        calves_list_status = requests.get("{url}/calves".format(url=base_url),headers = auth)
+    else:
+        return redirect(url_for('login'))
+    return calves_list_status.text
+
+@app.route('/farms/<farm_id>')
+def farms_cows():
+    auth = {}
+    if session.has_key('token') == True:
+        auth['Authorization'] = session['token']
+        calves_list_status = requests.get("{url}/farms/{farm_id}?include=calves".format(url=base_url, farm_id=farm_id),headers = auth)
+    else:
+        return redirect(url_for('login'))
+    return farms_cows_status.text
 
 # route for handling displaying list of farms
 @app.route('/farms')
 def farmList():
     auth = {}
-    auth['Authorization'] = session['token']
-    farm_list_status = requests.get("{url}/farms".format(url=base_url),headers = auth)
+    if session.has_key('token') == True:
+        auth['Authorization'] = session['token']
+        farm_list_status = requests.get("{url}/users/{id}?include=vet_for".format(url=base_url, id=session['id']), headers=auth)
+    else:
+        return redirect(url_for('login'))
     return farm_list_status.text
+#    return render_template('farms_list.html', data=json.dumps(farm_list_status.text))
 
 # start the server with the 'run()' method
 if __name__ == '__main__':
--- test.py
+++ test.py
@@ -1,1 +1,42 @@
 import json
+import requests
+from flask import Flask, session
+import pprint
+
+base_url = 'http://localhost:10200/v1'
+data={}
+data['email']='allen@example.com'
+data['password']='allen'
+login_status = requests.post("{url}/token?include=user".format(url=base_url), json=data)
+token = json.loads(login_status.text)['data']['id']
+auth = {}
+auth['Authorization']=token
+
+##Setting vet's farm
+# farm = {
+# 	'data':{
+# 		'type':'farms',
+# 		'attributes':{
+# 			'name':'farm3'
+# 		},
+# 		'relationships':{
+# 			'veterinarian':
+# 				{'data': [{
+# 					'type':'users',
+# 					'id': data['email']}]
+# 				}
+# 		}
+# 	}
+# }
+# farm_status = requests.post("{url}/farms".format(url=base_url), json = farm, headers = auth)
+# print farm_status
+# farm_request = requests.get("{url}/farms".format(url=base_url), headers = auth)
+# print farm_status.text
+#print(json.dumps(json.loads(farm_request.text),indent = 4, sort_keys=True))
+
+##Getting specific vet's farms
+# farm_request = requests.get("{url}/users/{id}?include=vet_for".format(url=base_url, id=data['email']), headers=auth)
+# print farm_request.text
+
+farm_cow_request = requests.get("{url}/farms/{id}?include=calves".format(url=base_url, id='1ph5f'), headers=auth)
+print farm_cow_request.text
