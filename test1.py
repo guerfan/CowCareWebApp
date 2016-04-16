823dd30dfd8e5aa6282294219d62f7181304abfb
Allen Yu <Allen Yu>
2016-04-15 20:16:44 -0400

    working post command. Screwed up edit.

--- test1.py
+++ test1.py
@@ -13,95 +13,10 @@ auth = {}
 auth['Authorization']=token
 
 # Post Treatment plan attemp
-json = {
-   "8b0f1293-4e45-4971-9aa0-ac7b2ae70154": {
-      "wait": 0,
-      "children": [
-         "59811a40-c9e8-4bb4-830a-e477df714f75"
-      ],
-      "description": "No medications on board, fit for transport",
-      "icon": "http://example.com/no_med_transportable.jpg"
-   },
-   "head": "86937ab-9840-4c5d-b29f-cee613743676",
-   "d1ccebc3-48b1-45af-9075-d33f64e26e9d": {
-      "wait": 0,
-      "children": [
-         "81c64ff9-c354-4fc9-b8d1-dd1a3986fed8"
-      ],
-      "description": "Morbund or unfit for slaughter",
-      "icon": "http://example.com/moribund.jpg"
-   },
-   "a388a993-a4ee-40b5-8629-3b21e29c480e": {
-      "wait": 0,
-      "children": [
-         "81c64ff9-c354-4fc9-b8d1-dd1a3986fed8"
-      ],
-      "description": "No response to treatment, condition declining",
-      "icon": "http://example.com/no_response_to_treatment.jpg"
-   },
-   "9cbe298c-660f-4a88-a43b-36fe0ae0c47c": {
-      "wait": 0,
-      "children": [
-         "d4430ee2-f33e-4798-bbd4-fdda82cbc665"
-      ],
-      "description": "Positive response to treatment, condition improves",
-      "icon": "http://example.com/positive_response_to_treatment.jpg"
-   },
-   "648f01bf-8398-4d5d-bcf0-bea6bcc0b436": {
-      "wait": 0,
-      "children": [],
-      "description": "Unfit for transport",
-      "icon": "http://example.com/untransportable.jpg"
-   },
-   "e0915e62-c2e5-442b-90d7-f040952169b0": {
-      "wait": 48,
-      "children": [
-         "a388a993-a4ee-40b5-8629-3b21e29c480e",
-         "9cbe298c-660f-4a88-a43b-36fe0ae0c47c"
-      ],
-      "description": "Separated for treatment or observation",
-      "icon": "http://example.com/separated.jpg"
-   },
-   "59811a40-c9e8-4bb4-830a-e477df714f75": {
-      "wait": 0,
-      "children": [
-
-      ],
-      "description": "Send for slaughter",
-      "icon": "http://example.com/send_for_slaughter.jpg"
-   },
-   "81c64ff9-c354-4fc9-b8d1-dd1a3986fed8": {
-      "wait": 0,
-      "children": [
-
-      ],
-      "description": "Euthanize immediately",
-      "icon": "http://example.com/euthanize.jpg"
-   },
-   "d4430ee2-f33e-4798-bbd4-fdda82cbc665": {
-      "wait": 0,
-      "children": [
-
-      ],
-      "description": "Continue treatment, return animal to group",
-      "icon": "http://example.com/continue_treatment_and_return.jpg"
-   },
-   "86937ab-9840-4c5d-b29f-cee613743676": {
-      "wait": 0,
-      "children": [
-         "d1ccebc3-48b1-45af-9075-d33f64e26e9d",
-         "e0915e62-c2e5-442b-90d7-f040952169b0",
-         "8b0f1293-4e45-4971-9aa0-ac7b2ae70154",
-         "648f01bf-8398-4d5d-bcf0-bea6bcc0b436"
-      ],
-      "description": "head",
-      "icon": None
-   }
-}
 treatment_plan={
 	'data':{
-   "id": "64c15",
    "type": "treatment_plans",
+   'attributes':{
    "title": "Treating Ail Cows",
    "body": {
       "head": "86937ab-9840-4c5d-b29f-cee613743676",
@@ -165,11 +80,12 @@ treatment_plan={
          "icon": "null",
          "children": ["d1ccebc3-48b1-45af-9075-d33f64e26e9d", "e0915e62-c2e5-442b-90d7-f040952169b0", "8b0f1293-4e45-4971-9aa0-ac7b2ae70154", "648f01bf-8398-4d5d-bcf0-bea6bcc0b436"]
       }
+      }
    }
 }
 }
 tp_post = requests.post("{url}/treatment_plans".format(url=base_url), headers=auth, json=treatment_plan);
-print tp_post.text
+print tp_post.status_code
 
 # # Retrive treatment plan
 # tp_status = requests.get("{url}/treatment_plans".format(url=base_url),headers = auth)
