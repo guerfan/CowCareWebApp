5660f00e11c840976b4735bccb82f87cd76f53fd
buckmaxwell <buck.169@osu.edu>
2016-04-21 12:42:33 -0400

    fixed bug where inactive treamtment plans were displayed...

--- app.py
+++ app.py
@@ -86,6 +86,7 @@ def treatmentplanList():
         if treatment_plan_status.status_code!= 200:
             return redirect(url_for('login'))
         json_treatment_list = json.loads(treatment_plan_status.text).get('included',[])
+        json_treatment_list = [x for x in json_treatment_list if x["data"]["attributes"]["active"]]
         if request.method == "POST":
             new_treatment_attributes = request.get_json()
             data ={
@@ -101,6 +102,7 @@ def treatmentplanList():
                 return redirect(url_for('login'))
             #json_treatment_list = json.loads(treatment_plan_status.text)['data']
             json_treatment_list = json.loads(treatment_plan_status.text).get('included', [])
+            json_treatment_list = [x for x in json_treatment_list if x["data"]["attributes"]["active"]]
             return render_template('treatment_list.html', treatment=json_treatment_list)
         if request.method == "DELETE":
             delete_json = request.get_json();
